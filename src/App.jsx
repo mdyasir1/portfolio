a00f2217a16129d88e25c3c02544bfa3a98d6964
mdyasir1 <mdyasir4145@gmail.com>
2025-03-27 20:23:16 +0530

    solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
